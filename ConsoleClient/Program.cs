f9c70147d5900e654d6ffe3c53cb339d6f6e8a57
Thomas Wycichowski <wycichowski@live.de>
2018-07-09 23:29:04 +0200

    Finish

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateUser(new SharedModels.ViewModels.RegisterViewModel() { Email = "wycichowski@gmail.com", Password = "Test123!" }).Wait();
+            var token = GetToken(new SharedModels.ViewModels.LoginViewModel() { Email = "wycichowski@gmail.com", Password = "Test123!" }).Result;
+            GetUserInfo(token).Wait();
+        }
+
+        static async Task CreateUser(SharedModels.ViewModels.RegisterViewModel model)
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(model);
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            StringContent theContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var resp = await client.PostAsync("http://localhost:5000/api/auth/register", theContent);
+        }
+
+        static async Task<string> GetToken(SharedModels.ViewModels.LoginViewModel model)
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(model);
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            StringContent theContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var resp = await client.PostAsync("http://localhost:5000/api/auth/login", theContent);
+            if (resp.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return await resp.Content.ReadAsStringAsync();
+            }
+
+            return "";
+        }
+
+        static async Task GetUserInfo(string token)
+        {
+            //Trim first and last "
+            var t = token.Substring(1, token.Length - 2);
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", t);
+
+            var resp = await client.GetAsync("http://localhost:5000/api/auth/test");
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SharedModels.Data;
+using SharedModels.Models;
+
+namespace SharedModels
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppUsersDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppUsersDbContext>()
+                .AddDefaultTokenProviders();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AppUsersDbContext db)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Create Database
+            db.Database.EnsureCreated();
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
